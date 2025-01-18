11e3402936a5417b09109deab1311612b721dc0b
Yaswant Singh <yaswant@gmail.com>
2025-01-19 01:32:00 +0530

    Added the init logic for add method

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,5 +1,12 @@
 class Calculator < ApplicationRecord
     def self.add(str_nums)
-      return 0
+        return 0 if str_nums.empty?
+
+        begin
+          number_list = str_nums.split(',').map(&:to_i)
+          number_list.sum
+        rescue ArgumentError
+          raise ArgumentError, "Invalid input: Ensure all items are separated by commas."
+        end
     end
 end
