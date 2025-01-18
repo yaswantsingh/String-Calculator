30af17910ec1357a68137b06c43fe82f865839fe
Yaswant Singh <yaswant@gmail.com>
2025-01-19 01:08:07 +0530

    Added the basic unit test cases as per TDD

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,3 +1,5 @@
 class Calculator < ApplicationRecord
-    
+    def self.add(str_nums)
+      return 0
+    end
 end
--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class CalculatorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_empty_string
+    assert_equal 0, Calculator.add("")
+  end
+
+  def test_single_number
+    assert_equal 1, Calculator.add("1")
+  end
+
+  def test_two_numbers
+    assert_equal 6, Calculator.add("2,6")
+  end
+
+  def test_multiple_numbers
+    assert_equal 15, Calculator.add("1,2,3,4,5")
+  end
+
+  def test_invalid_input
+    assert_raises(ArgumentError) { Calculator.add() }
+  end
 end
